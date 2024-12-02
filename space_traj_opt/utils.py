b049e031bfcc5e9fe1f94be9b4cf0abed08c35a3
hassanarif87 <hassanarif87@gmail.com>
2024-12-01 22:37:20 -0800

    Normalization functions (#3)
    * Adding normalizaton functions
    
    * Adding denormalize method
    
    * Split dynamics, jit working
    
    * moving unpack to utils
    
    * clean up
    
    * lru cache working
    
    * re name
    
    * lock file update

--- space_traj_opt/utils.py
+++ space_traj_opt/utils.py
@@ -1,5 +1,6 @@
 from timeit import default_timer
 from functools import wraps
+from scipy.integrate import  OdeSolution
 
 def vocalTimeit(*args, **kwargs):
     ''' provides the decorator @vocalTime which will print the name of the function as well as the
@@ -15,3 +16,23 @@ def vocalTimeit(*args, **kwargs):
             return results
         return wrapper
     return decorator
+
+def unpack_sol_list(sol_list_in: list[OdeSolution] , state_index: list[int])-> tuple[list, list]:
+    """Helper function to add time offsets to each phase
+
+    Args:
+        sol_list_in : OdeSoluion for each phase
+        state_index (_type_): Index of the state to unpack
+
+    Returns:
+        tuple of lists of time arrays and lists of the state arrays for each phase S
+    """
+    t_offsets = 0
+    x_list = []
+    y_list = []
+    for sol in sol_list_in:
+        x_list.append(sol.t + t_offsets )
+        y_list.append(sol.y[state_index] )
+        t_offsets += sol.t[-1]
+
+    return x_list, y_list
